ba71b3c9f1b24bbe9dbd953cd293e92dc2773772
ECON <lgyoon621@naver.com>
2025-09-28 04:25:59 +0900

    feat: QueryDSL 설정 및 종속성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,8 +25,15 @@ repositories {
 }
 
 dependencies {
+    //Querydsl
+    implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
+    annotationProcessor("com.querydsl:querydsl-apt:5.1.0:jakarta")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation ("org.springframework.boot:spring-boot-starter-validation")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 //    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
