9e3f948b04ab08d35f4b6b75ebf29dc9c40d06a2
ECON <lgyoon621@naver.com>
2025-09-28 04:25:05 +0900

    feat: 데이터 베이스 스키마 파일 수정

--- ddl.sql
+++ ddl.sql
@@ -1,14 +1,16 @@
+CREATE EXTENSION IF NOT EXISTS btree_gist;
+
 CREATE TABLE IF NOT EXISTS "rooms" (
-	"id" serial NOT NULL UNIQUE,
+	"id" bigserial NOT NULL UNIQUE,
 	"user_id" bigint NOT NULL,
 	"name" varchar(255) NOT NULL,
-	"capacity" bigint NOT NULL,
-	"addresss" varchar(255) NOT NULL,
+	"capacity" int NOT NULL,
+	"address" varchar(255) NOT NULL,
 	PRIMARY KEY ("id")
 );
 
 CREATE TABLE IF NOT EXISTS "users" (
-	"id" serial NOT NULL UNIQUE,
+	"id" bigserial NOT NULL UNIQUE,
 	"login_id" varchar(255) NOT NULL,
 	"login_pw" varchar(255) NOT NULL,
 	"name" varchar(255) NOT NULL,
@@ -16,13 +18,18 @@ CREATE TABLE IF NOT EXISTS "users" (
 	PRIMARY KEY ("id")
 );
 
+-- TODO btree_gist 확장 + EXCLUDE 제약, tstzrange 사용
 CREATE TABLE IF NOT EXISTS "reservations" (
-	"id" serial NOT NULL UNIQUE,
+	"id" bigserial NOT NULL UNIQUE,
 	"room_id" bigint NOT NULL,
 	"user_id" bigint NOT NULL,
-	"start_at" varchar(255) NOT NULL,
-	"end_at" varchar(255) NOT NULL,
-	"created_at" varchar(255) NOT NULL,
+	"start_at" TIMESTAMPTZ  NOT NULL,
+	"end_at" TIMESTAMPTZ  NOT NULL,
+	"created_at" TIMESTAMPTZ  NOT NULL,
+    EXCLUDE USING gist(
+        room_id WITH =,
+        tstzrange(start_at, end_at, '[)') WITH &&
+        ),
 	PRIMARY KEY ("id")
 );
 
@@ -31,3 +38,5 @@ ALTER TABLE "rooms" ADD CONSTRAINT "rooms_fk1" FOREIGN KEY ("user_id") REFERENCE
 ALTER TABLE "reservations" ADD CONSTRAINT "reservations_fk1" FOREIGN KEY ("room_id") REFERENCES "rooms"("id");
 
 ALTER TABLE "reservations" ADD CONSTRAINT "reservations_fk2" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+
+SET TIME ZONE 'Asia/Seoul';
